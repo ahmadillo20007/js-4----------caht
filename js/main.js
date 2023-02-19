994840e67235c4fe6e042e718dbbe0b67ed33094
Ahmadillo20007 <ahmadilloqubijonov@gmail.com>
2023-02-19 10:00:14 +0500

    e

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let num1 = parseInt(prompt("1-sonni kiriting: "));
 let num2 = parseInt(prompt("2-sonni kiriting: "));
 let num3 = parseInt(prompt("3-sonni kiriting: "));
+let elText = document.querySelector('.text')
 
 function min(a, b, c) {
   return Math.min(a, Math.min(b, c));
@@ -8,4 +9,22 @@ function min(a, b, c) {
 
 let minNum = min(num1, num2, num3);
 
-console.log("Eng kichik son: " + minNum);
+elText.innerHTML = "Eng kichik son: " + minNum;
+
+// qwertyuiopasdfghjk
+
+
+let counter = 0;
+const counterElement = document.querySelector('.counter');
+
+function increaseCounter() {
+  counter++;
+  counterElement.innerHTML = counter;
+}
+
+function resetCounter() {
+  counter = 0;
+  counterElement.innerHTML = counter;
+}
+
+
